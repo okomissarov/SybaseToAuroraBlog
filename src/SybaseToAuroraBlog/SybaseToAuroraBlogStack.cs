ff357e5472ded5e0c9a0dc1b18de966be6b5bf8c
okomissarov <olegkomissarov@gmail.com>
2023-04-29 09:20:08 -0400

    Initial commit

---
+++ src/SybaseToAuroraBlog/SybaseToAuroraBlogStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace SybaseToAuroraBlog
+{
+    public class SybaseToAuroraBlogStack : Stack
+    {
+        internal SybaseToAuroraBlogStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
